8be1aeb3788e29c6ac0ad89aee74becf45ec2bdd
ikrsx <krishal.sh@tuta.io>
2025-09-20 16:24:06 +0530

    updated bash.nix

--- knight/settings/bash.nix
+++ knight/settings/bash.nix
@@ -2,7 +2,7 @@
  programs.bash.shellAliases = {
    ls = "ls --color --sort extension";
    nc = "sudo nvim /etc/nixos/";
-   nrs = "sudo nixos-rebuild switch";
+   nrs = "sudo nixos-rebuild switch --flake ~/Workstation/nix-dotfiles#krisnix";
    ncg = "sudo nix-collect-garbage -d";
  };
  programs.bash.initExtra = ''
