c243211b63039869b9e0d4989959dba0c4f74b76
ikrsx <krishal.sh@tuta.io>
2025-09-21 13:16:06 +0530

    updated packages.nix & miscellaneous.nix

--- krisnix/categories/miscellaneous.nix
+++ krisnix/categories/miscellaneous.nix
@@ -3,8 +3,8 @@
   programs.nano.enable = false;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # virtualisation.containers.enable = true;
-  # virtualisation.podman.enable = true;
-  # virtualisation.podman.dockerCompat = true;
-  # virtualisation.podman.defaultNetwork.settings.dns_enable = true;
+  virtualisation.containers.enable = true;
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerCompat = true;
+  virtualisation.podman.defaultNetwork.settings.dns_enable = true;
 }
--- krisnix/categories/packages.nix
+++ krisnix/categories/packages.nix
@@ -24,7 +24,7 @@
      bleachbit
      libreoffice-fresh
      firefox
-     #distrobox
+     distrobox
   ];
   
   fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
